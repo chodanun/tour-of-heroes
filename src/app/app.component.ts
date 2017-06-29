b71627ccb8df219974ddcf4b896ed97b3b812c9f
Chodanun Srinil <step_nilanam@hotmail.com>
2017-06-29 15:48:42 +0700

    change template root-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+		<h1>{{title}}</h1>
+		<h2>{{hero.name}} details!</h2>
+		<div><label>id: </label>{{hero.id}}</div>
+		<div><label>name: </label>{{hero.name}}</div>
+  `,
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Tour Of Heroes';
+
+  hero: Hero = {
+  	id: 1,
+  	name: 'Windsotorm',
+  }
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
